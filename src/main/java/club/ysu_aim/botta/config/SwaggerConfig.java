018cea0ee6423d355c2d87ecb68bfdb2a2514af7
김정욱 <yeoback07@icloud.com>
2026-07-08 15:26:23 +0900

    [Feat]: Swagger를 활용한 API 명세서 셋업 - 종속성 추가 - 테스트 컨트롤러 작성(삭제해도 무관)

---
+++ src/main/java/club/ysu_aim/botta/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package club.ysu_aim.botta.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My AWESOME API 명세서")
+                        .description("Spring Boot 기반의 백엔드 API 문서입니다.")
+                        .version("0.0.1-PRE-ALPHA"));
+    }
+}

be13f3b8e45034c5189d944555dc84074fb05dec
김정욱 <yeoback07@icloud.com>
2026-07-09 13:32:13 +0900

    Chore: 서버 설정 변경 - 학교 DB, Redis 서버에 접속 가능하게 변경 - docker compose에 db, redis 관련한 구문 삭제 - Redis 종속성 추가 - Postgres, Redis 접속 성공 시 로그 띄움

---
+++ src/main/java/club/ysu_aim/botta/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package club.ysu_aim.botta.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger springdoc-ui 구성 파일
+ */
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Ysu Aim Botta API")
+                .version("v0.0.1-PRE_ALPHA")
+                .description("Ysu Aim Botta API");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}

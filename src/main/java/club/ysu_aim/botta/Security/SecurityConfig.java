714a969864dea6eabe312f282fced9549c5b7cc2
jehee826 <jehee826@gmail.com>
2026-08-12 16:05:59 +0900

    [Fix, Chore]: Swagger-Ui설정 변경, SecurityConfig설정 변경, NoteBookDTO코드 수정
    -SwaggerUi에서 HTTPS요청을 받을수있게 변경
    -CORS설정에서 CloudFlare주소추가
    -NotebookDTO코드에서 @NotBlank어노테이션 위치 변경

--- src/main/java/club/ysu_aim/botta/Security/SecurityConfig.java
+++ src/main/java/club/ysu_aim/botta/Security/SecurityConfig.java
@@ -56,7 +56,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173","http://localhost:8080"));
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:3000", "http://localhost:5173", "http://localhost:8080", "https://*.trycloudflare.com"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 수용
         configuration.setAllowCredentials(true);
